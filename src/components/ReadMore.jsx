3c2dbf9317778991a4a298ce3928262d8be2a7bc
dlouise64 <dana.iti.work@gmail.com>
2020-02-17 21:57:19 +1300

    use button instead of span so they're reachable via tab key presses

--- src/components/ReadMore.jsx
+++ src/components/ReadMore.jsx
@@ -1,30 +1,38 @@
-import React from "react";
+import React from "react"
 
 const ReadMore = props => {
-	const [textStyle, setTextStyle] = React.useState(null);
-	const [triggerStyle, setTriggerStyle] = React.useState(null);
+  const [textStyle, setTextStyle] = React.useState(null)
+  const [triggerStyle, setTriggerStyle] = React.useState(null)
 
-	const showContent = e => {
-		e.preventDefault();
-		if (e) {
-			setTextStyle({ height: "auto" });
-			setTriggerStyle({ display: "none" });
-		}
-	};
+  const showContent = e => {
+    e.preventDefault()
+    if (e) {
+      setTextStyle({ height: "auto" })
+      setTriggerStyle({ display: "none" })
+    }
+  }
 
-	return (
-		<p>
-			<span className="text" id="text" style={textStyle}>
-				{props.text}
-			</span>
-			<span className="readmore" id="readmore" style={triggerStyle}>
-				...
-				<span className="readmore-text" onClick={showContent}>
-					Read More
-				</span>
-			</span>
-		</p>
-	);
-};
+  return (
+    <p>
+      <span className="text" id="text" style={textStyle}>
+        {props.text}
+      </span>
+      <span className="readmore" id="readmore" style={triggerStyle}>
+        ...
+        {/* <span
+          role="link"
+          className="readmore-text"
+          onClick={showContent}
+          onKeyDown={showContent}
+        >
+          Read More
+        </span> */}
+        <button className="readmore-text" onClick={showContent}>
+          Read More
+        </button>
+      </span>
+    </p>
+  )
+}
 
-export default ReadMore;
+export default ReadMore
