289dd1d0c375f02159c3a1d5790cca660c7b04fe
dlouise64 <dana.iti.work@gmail.com>
2020-02-17 21:44:39 +1300

    onBlur must be used instead of onchange for selects

--- src/components/AdvancedSearch/Court.jsx
+++ src/components/AdvancedSearch/Court.jsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React from "react"
 
 const Court = ({ value, id, onChange, className }) => (
-	<div className={className}>
-		<select
-			id={`simple-${id}`}
-			onChange={ev => onChange({ id, value: ev.target.value })}
-			value={value}
-			required
-			className="extended-select"
-		>
-			<option value="">Select an option</option>
-			<option value="Supreme Court">Supreme Court</option>
-			<option value="Court of appeal">Court of Appeal</option>
-			<option value="High Court">High Court</option>
-			<option value="District Court">District Court</option>
-		</select>
-	</div>
-);
+  <div className={className}>
+    <select
+      id={`simple-${id}`}
+      onBlur={ev => onChange({ id, value: ev.target.value })}
+      value={value}
+      required
+      className="extended-select"
+    >
+      <option value="">Select an option</option>
+      <option value="Supreme Court">Supreme Court</option>
+      <option value="Court of appeal">Court of Appeal</option>
+      <option value="High Court">High Court</option>
+      <option value="District Court">District Court</option>
+    </select>
+  </div>
+)
 
-export default Court;
+export default Court
