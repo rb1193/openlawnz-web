fe2ea6a536b4b3ae7442bdd521d42defdd3c215f
William Parry <williamparry@gmail.com>
2020-02-24 23:30:09 +1100

    Another attempt

--- src/pages/case.js
+++ src/pages/case.js
@@ -1,17 +1,16 @@
 import React from "react"
-import Layout from "../components/layout"
 import { Router } from "@reach/router"
 
 import SingleCaseContainer from "../components/SingleCaseContainer";
-import NotFound from "./404";
+
 
 export default () => {
   return (
-    <Layout>
+    
       <Router basepath="/case">
         <SingleCaseContainer path="/:id" />
-        <NotFound path="/" />
+
       </Router>
-    </Layout>
+   
   )
 }
