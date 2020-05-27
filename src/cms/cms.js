141ecd0e7db56ee7aba24a15a835bd3c15113fc2
Ben Rauzi <ben.rauzi@outlook.com>
2020-05-27 20:24:57 +1200

    Tidy Up Search Result/Case View (Not to new style, just made it usable) and changed environment variable names

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,17 +2,17 @@ import { init } from "netlify-cms-app";
 
 window.CMS_MANUAL_INIT = true;
 
-const { BRANCH } = process.env; 
+const { GATSBY_BRANCH } = process.env; 
 
-  const config = {
-    backend: {
-      name: "git-gateway",
-      repo: "openlaw-web",
-      branch: BRANCH || "master" 
-    },
-    media_folder: "static/assets",
-    local_backend: true,
-    public_folder: "/assets"
-  };
+const config = {
+  backend: {
+    name: "git-gateway",
+    repo: "openlaw-web",
+    branch: GATSBY_BRANCH || "master" 
+  },
+  media_folder: "static/assets",
+  public_folder: "/assets",
+  local_backend: true
+};
 
-  init({ config });
+init({ config });
