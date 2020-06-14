9d8346a45e310cc2e6a5ce466b54609221a927f9
Ben Rauzi <ben.rauzi@outlook.com>
2020-06-14 19:27:41 +1200

    Set display to standalone in gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#2b6064`,
         theme_color: `#2b6064`,
-        display: `fullscreen`,
+        display: `standalone`,
         icon: `src/images/logo-small.png`, // This path is relative to the root of the site.
       },
     },
