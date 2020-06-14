aef296dc5a8e2a81565a46fee0d1c4d4e58a1a6f
Ben Rauzi <ben.rauzi@outlook.com>
2020-06-14 18:09:27 +1200

    Change Address Bar Colour (Mobile)

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2b6064`,
+        theme_color: `#2b6064`,
         display: `minimal-ui`,
         icon: `src/images/logo-small.png`, // This path is relative to the root of the site.
       },
