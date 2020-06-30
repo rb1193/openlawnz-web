d281b1ec3fe6c91193cd5f02ff1dd1f23d7fbb8b
Ben Rauzi <ben.rauzi@outlook.com>
2020-06-30 21:47:52 +1200

    Simplfiy GetInvolvedPageContent

--- src/templates/get-involved-page.js
+++ src/templates/get-involved-page.js
@@ -2,21 +2,19 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import ModuleSelector from "../components/ModuleSelector"
-export const GetInvolvedPageContent = ({ pageContext }) => {
-  return (
-    <div className="container main">
-      <div className="content">
-        <h1>{pageContext.title}</h1>
-        {pageContext.content.map((module, idx) => {
-          return (
-            <div key={idx}>
-              <ModuleSelector module={module} wizardModuleData={pageContext.wizardData} idx={idx}/>
-            </div>
-        )})}
-      </div>
+export const GetInvolvedPageContent = ({ pageContext }) => (
+  <div className="container main">
+    <div className="content">
+      <h1>{pageContext.title}</h1>
+      {pageContext.content.map((module, idx) => {
+        return (
+          <div key={idx}>
+            <ModuleSelector module={module} wizardModuleData={pageContext.wizardData} idx={idx}/>
+          </div>
+      )})}
     </div>
-  )
-}
+  </div>
+)
 
 const GetInvolvedPage = ({ pageContext }) => (
   <Layout>
