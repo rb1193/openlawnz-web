0139e5a9aa9f2f1c06ec6f83ad76a8277cbb8ca4
Ben Rauzi <ben.rauzi@outlook.com>
2020-07-12 23:43:54 +1200

    Add Basic Mobile Nav

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,13 +1,38 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 import Logo from "../images/svgs/openlaw-logo.svg"
 import SearchContainer from "./SearchContainer"
+import {toSlug} from "../js/ToSlug"
 
 
 const MainNav = () => {
-  const [isNavOpen, setIsNavOpen] = React.useState(false)
-  const toggleNavState = () => setIsNavOpen(!isNavOpen)
+
+  const { allMicrositesJson, allGetInvolvedJson } = useStaticQuery(
+    graphql`
+      query {
+        allMicrositesJson {
+          nodes {
+            title
+            fields {
+              slug
+            }
+            content {
+              title
+            }
+          }
+        }
+        allGetInvolvedJson {
+          nodes {
+            title
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    `
+  )
 
   return (
     <header role="banner" id="nav" className="nav-container">
@@ -28,8 +53,6 @@ const MainNav = () => {
           <input
               aria-hidden
               type="checkbox"
-              checked={isNavOpen}
-              onChange={toggleNavState}
           />
           <span></span>
           <span></span>
@@ -40,9 +63,48 @@ const MainNav = () => {
             <nav>
               <ul>
                 <li><Link to="/our-mission" partiallyActive={true} activeClassName="link-active"  >Our Mission</Link></li>
-                <li><Link to="/get-empowered" partiallyActive={true} activeClassName="link-active">Get Empowered</Link></li>
-                <li><Link to="/get-involved" partiallyActive={true} activeClassName="link-active">Get Involved</Link></li>
-                <li><Link activeClassName="link-active" partiallyActive={true} to="/news">News</Link></li>
+                <li>
+                  <Link to="/get-empowered" partiallyActive={true} activeClassName="link-active">Get Empowered</Link>
+                    <input
+                      aria-hidden
+                      type="checkbox"
+                    />
+                    <span></span>
+                    <span></span>
+
+                  <ul className="mobile-secondary">
+                    {
+                      allMicrositesJson.nodes.map(({title, fields, content}, idx) => {
+                        return (
+                          <li key={idx}>
+                            <Link to={`/get-empowered${fields.slug}${toSlug(content[0].title)}`}>{title}</Link>
+                          </li>
+                        )
+                    })
+                    }
+                  </ul>
+                </li>
+                <li><Link to="/get-involved" partiallyActive={true} activeClassName="link-active">Get Involved</Link>
+                  <input
+                    aria-hidden
+                    type="checkbox"
+                  />
+                  <span></span>
+                  <span></span>
+                
+                  <ul className="mobile-secondary">
+                    {
+                      allGetInvolvedJson.nodes.map(({title, fields}, idx) => {
+                        return (
+                          <li key={idx}>
+                            <Link to={`/get-involved${fields.slug}`}>{title}</Link>
+                          </li>
+                        )
+                    })
+                    }
+                  </ul>
+                </li>
+                <li><Link to="/news" partiallyActive={true} activeClassName="link-active">News</Link></li>
               </ul>
             </nav>
            
