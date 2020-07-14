1835c9f85232f179a0d9ef94744afb57e11793aa
Ben Rauzi <ben.rauzi@outlook.com>
2020-07-14 20:29:09 +1200

    Tidy up CMS styling for demo

--- src/templates/get-involved-page.js
+++ src/templates/get-involved-page.js
@@ -4,17 +4,15 @@ import SEO from "../components/seo"
 import ModuleSelector from "../components/ModuleSelector"
 
  export const GetInvolvedPageContent = ({ pageContext }) => (
-  <div className="side-wrapper">
-     <div className="container-wide main">
-      <div className="content">
-        <h1>{pageContext.title}</h1>
-        {pageContext.content.map((module, idx) => {
-          return (
-            <div key={idx}>
-              <ModuleSelector module={module} wizardModuleData={pageContext.wizardData} idx={idx}/>
-            </div>
-        )})}
-      </div>
+  <div className="container-wide main">
+    <div className="content">
+      <h1>{pageContext.title}</h1>
+      {pageContext.content.map((module, idx) => {
+        return (
+          <div key={idx}>
+            <ModuleSelector module={module} wizardModuleData={pageContext.wizardData} idx={idx}/>
+          </div>
+      )})}
     </div>
   </div>
 )
