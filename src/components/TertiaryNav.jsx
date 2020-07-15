2fb64da61080d3361ead5ced511a6da9ad73e4d4
Ben Rauzi <ben.rauzi@outlook.com>
2020-07-15 12:46:28 +1200

    Fix Anchor Links, Add 'Using OpenLawNZ Sections' to 'Get Empowered' Page

--- src/components/TertiaryNav.jsx
+++ src/components/TertiaryNav.jsx
@@ -3,6 +3,7 @@ import React from "react"
 import { Link } from "gatsby"
 import { toSlug } from "../js/ToSlug"
 
+
 const TertiaryNav = props => {
   return (
     <div className="tertiary-nav">
@@ -22,11 +23,11 @@ const TertiaryNav = props => {
                     content[0] === props.page &&
                       <ul className="tertiary-sub-routes">
                         {
-                          props.secondary_data.map((x, idx) => {
+                          props.secondary_data.map((title, idx) => {
                             return (
                               <li key={idx}>
-                                <Link  to={(props.base + "/" + toSlug(content[1])) + "#" + toSlug(x)} key={idx}>
-                                 {x}
+                                <Link  to={(props.base + "/" + toSlug(content[1])) + "#" + toSlug(title)} key={idx}>
+                                 {title}
                                 </Link>
                               </li>
                             )  
@@ -37,6 +38,24 @@ const TertiaryNav = props => {
               </div>
             )
           })
+        }
+
+        { //Used in the 'Get Empowered' Page.
+          props.tertiary_data !== undefined &&
+          <div className="secondary-menu">
+            {
+              props.tertiary_data.map((content, idx) => (
+                <li className="tertiary-item" key={idx}>
+                  <Link to={(props.base + "/" + toSlug(content[1]))}
+                  activeClassName="tertiary-active">
+    
+                  {content[0]}
+                  </Link>
+                </li>
+              ))
+            }
+          </div>
+          
         }
       </ul>
     </div>
